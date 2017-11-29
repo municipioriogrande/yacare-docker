dd1c5f1f9301ddd17ecd3ab87072f7be8c7de094
friasdesign <cfrias@localhost.localdomain>
2017-11-29 16:40:52 -0300

    Agrega comando para construir todas las imagenes Docker de forma recursiva

---
+++ build-all.sh
@@ -0,0 +1,18 @@
+# Create images for each subfolder tagging them without versioning. Default Docker namespace is `carlosafrias`.
+clear
+if [ -z "$1" ]
+    then
+        NAMESPACE="carlosafrias"
+    else
+        NAMESPACE=$1
+fi
+echo "Building images on all subdirectories..."
+
+for D in *; do
+    if [ -d "${D}" ] && [ -s "${D}/Dockerfile" ]; then
+        echo "  -- Building image for ${D} ..."
+        docker build -t ${NAMESPACE}/yacare:${D} ${D}/
+    fi
+done
+
+echo "Done!"
