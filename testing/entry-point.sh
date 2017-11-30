2b208dc067a9b9e0e8643112626626ae0db5afe2
friasdesign <cfrias@localhost.localdomain>
2017-11-30 15:33:08 -0300

    Change mv to cp

--- testing/entry-point.sh
+++ testing/entry-point.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd /home/test/yacare
-mv -f /home/test/parameters.yml app/config
+cp -f /home/test/parameters.yml app/config
 composer install
 php bin/console doctrine:schema:update --force --env=test
 
