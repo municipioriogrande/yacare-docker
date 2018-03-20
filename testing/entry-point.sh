d4c0488899e877cd08a37da156a2e2c8578cc882
friasdesign <carlos.a.frias@gmail.com>
2018-03-20 17:00:50 -0300

    Add codecept build to entry-point

--- testing/entry-point.sh
+++ testing/entry-point.sh
@@ -6,7 +6,6 @@ composer install
 php bin/console doctrine:schema:update --force --env=test
 
 # Update codeception helpers
-# echo "User: $USER. Username: $USERNAME"
-# /home/test/yacare/vendor/codeception/codeception/codecept build
+php vendor/codeception/codeception/codecept build
 
 exec "$@"
--- testing/run.sh
+++ testing/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker-compose -p yacare-testing run --rm yacare php /home/test/yacare/vendor/codeception/codeception/codecept run
+docker-compose -p yacaretesting run --rm yacare php /home/test/yacare/vendor/codeception/codeception/codecept run
