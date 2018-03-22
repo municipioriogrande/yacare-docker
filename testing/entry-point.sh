176c74290a7d3d21595dfa2d8217cf866fa22fe2
friasdesign <carlos.a.frias@gmail.com>
2018-03-22 11:29:04 -0300

    Add github Oauth

--- testing/entry-point.sh
+++ testing/entry-point.sh
@@ -2,6 +2,9 @@
 
 cd /root/yacare
 cp /root/parameters.yml /root/yacare/app/config/
+
+composer config --global github-oauth.github.com $GITHUB_TOKEN
+
 composer install
 php bin/console doctrine:schema:update --force --env=test
 
