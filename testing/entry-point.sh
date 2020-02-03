ecd70ab92bc4a6fc7778471b4cb71e3120632f46
Facundo <facuchaer@gmail.com>
2020-02-03 16:37:54 -0300

    intento de .env.test

--- testing/entry-point.sh
+++ testing/entry-point.sh
@@ -2,6 +2,7 @@
 
 cd /root/yacare
 cp /root/parameters.yml /root/yacare/app/config/
+cp /root/.env.test /root/yacare/
 
 composer config extra.symfony.allow-contrib true
 composer config --global github-oauth.github.com $GITHUB_TOKEN
